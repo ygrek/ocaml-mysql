37b17c20970af1f44c88d1a489652f920d38544a
ygrek <ygrek@autistici.org>
2015-02-08 16:52:38 +0800

    get rid of shtool (specify version in configure.in)

--- etc/get_version.ml
+++ etc/get_version.ml
@@ -1,10 +1,1 @@
-let () =
-  let ch = open_in "VERSION" in
-  while true do
-    let s = input_line ch in
-    try
-      Scanf.sscanf s " This is mysql, Version %s@ " print_endline;
-      exit 0
-    with
-      _ -> ()
-  done
+let () = print_endline (input_line (open_in "VERSION"))
